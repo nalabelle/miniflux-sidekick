b2e0e434f307d9f102aefdbfce9168bd2d64a0c8
nalabelle <git@nalabelle.com>
2023-11-02 00:25:04 -0700

    fix up imports

--- filter/service.go
+++ filter/service.go
@@ -7,7 +7,7 @@ import (
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	"github.com/nalabelle/miniflux-sidekick/rules"
-	miniflux "miniflux.app/client"
+	miniflux "miniflux.app/v2/client"
 )
 
 // Service is an interface for a filter service
