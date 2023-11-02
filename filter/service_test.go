09b16d097b99aee53525577b0d73a658af629ea2
nalabelle <git@nalabelle.com>
2023-11-01 23:40:17 -0700

    add an expression to filter on specific tags + remove portions of the original code I'm not using

--- filter/service_test.go
+++ filter/service_test.go
@@ -1,134 +1,116 @@
 package filter
 
 import (
-	"github.com/dewey/miniflux-sidekick/rules"
-	"github.com/go-kit/kit/log"
-	miniflux "miniflux.app/client"
 	"testing"
+
+	"github.com/go-kit/kit/log"
+	"github.com/nalabelle/miniflux-sidekick/rules"
+	miniflux "miniflux.app/client"
 )
 
 func TestEvaluateRules(t *testing.T) {
 	type mockService struct {
-		rules  []rules.Rule
-		l      log.Logger
+		rules []rules.Rule
+		l     log.Logger
 	}
 
 	tests := []struct {
-		name   string
-		rules []rules.Rule
-		args   *miniflux.Entry
-		want   bool
+		name       string
+		expression string
+		args       *miniflux.Entry
+		want       bool
 	}{
 		{
-			name: "Entry contains string",
-			rules: []rules.Rule{
-				{
-					Command: "ignore-article",
-					URL: "http://example.com/feed.xml",
-					FilterExpression: "title # Moon",
-				},
-			},
+			name:       "Entry contains string",
+			expression: "\"ignore-article\" \"http://example.com/feed.xml\" \"title # Moon\"",
 			args: &miniflux.Entry{
 				Title: "Moon entry",
 			},
 			want: true,
 		},
 		{
-			name: "Entry contains string",
-			rules: []rules.Rule{
-				{
-					Command: "ignore-article",
-					URL: "http://example.com/feed.xml",
-					FilterExpression: "title # Moon",
-				},
-			},
+			name:       "Entry contains string",
+			expression: "\"ignore-article\" \"http://example.com/feed.xml\" \"title # Moon\"",
 			args: &miniflux.Entry{
 				Title: "Sun entry",
 			},
 			want: false,
 		},
 		{
-			name: "Entry contains string, matched with Regexp",
-			rules: []rules.Rule{
-				{
-					Command: "ignore-article",
-					URL: "http://example.com/feed.xml",
-					FilterExpression: "title =~ [Sponsor]",
-				},
-			},
+			name:       "Entry contains string, matched with Regexp",
+			expression: "\"ignore-article\" \"http://example.com/feed.xml\" \"title =~ [Sponsor]\"",
 			args: &miniflux.Entry{
 				Title: "[Sponsor] Sun entry",
 			},
 			want: true,
 		},
 		{
-			name: "Entry doesn't string, matched with Regexp",
-			rules: []rules.Rule{
-				{
-					Command: "ignore-article",
-					URL: "http://example.com/feed.xml",
-					FilterExpression: `title =~ \[Sponsor\]`,
-				},
-			},
+			name:       "Entry doesn't string, matched with Regexp",
+			expression: "\"ignore-article\" \"http://example.com/feed.xml\" `title =~ \\[Sponsor\\]`",
 			args: &miniflux.Entry{
 				Title: "[SponSomethingElsesor] Sun entry",
 			},
 			want: false,
 		},
 		{
-			name: "Entry doesn't string, matched with Regexp, ignore case",
-			rules: []rules.Rule{
-				{
-					Command: "ignore-article",
-					URL: "http://example.com/feed.xml",
-					FilterExpression: "title =~ (?i)(Podcast|scooter)",
-				},
-			},
+			name:       "Entry doesn't string, matched with Regexp, ignore case",
+			expression: "\"ignore-article\" \"http://example.com/feed.xml\" \"title =~ (?i)(Podcast|scooter)\"",
 			args: &miniflux.Entry{
 				Title: "podcast",
 			},
 			want: true,
 		},
 		{
-			name: "Entry doesn't string, matched with Regexp, ignore case",
-			rules: []rules.Rule{
-				{
-					Command: "ignore-article",
-					URL: "http://example.com/feed.xml",
-					FilterExpression: "title =~ (?i)(Podcast|scooter)",
-				},
-			},
+			name:       "Entry doesn't string, matched with Regexp, ignore case",
+			expression: "\"ignore-article\" \"http://example.com/feed.xml\" \"title =~ (?i)(Podcast|scooter)\"",
 			args: &miniflux.Entry{
 				Title: "SCOOTER",
 			},
 			want: true,
 		},
 		{
-			name: "Entry doesn't string, matched with Regexp, respect case",
-			rules: []rules.Rule{
-				{
-					Command: "ignore-article",
-					URL: "http://example.com/feed.xml",
-					FilterExpression: "title =~ (Podcast)",
-				},
-			},
+			name:       "Entry doesn't string, matched with Regexp, respect case",
+			expression: "\"ignore-article\" \"http://example.com/feed.xml\" \"title =~ (Podcast)\"",
 			args: &miniflux.Entry{
 				Title: "podcast",
+			},
+			want: false,
+		},
+		{
+			name:       "Entry doesn't tag, case insensitive",
+			expression: "\"ignore-article\" \"http://example.com/feed.xml\" \"tag # (?i)podcast\"",
+			args: &miniflux.Entry{
+				Tags: []string{"this", "Podcast", "testing"},
+			},
+			want: true,
+		},
+		{
+			name:       "Entry doesn't tag, respect case",
+			expression: "\"ignore-article\" \"http://example.com/feed.xml\" \"tag # podcast\"",
+			args: &miniflux.Entry{
+				Tags: []string{"this", "Podcast", "testing"},
+			},
+			want: false,
+		},
+		{
+			name:       "Entry doesn't tag, miss",
+			expression: "\"ignore-article\" \"http://example.com/feed.xml\" \"tag # test\"",
+			args: &miniflux.Entry{
+				Tags: []string{"this", "podcast", "testing"},
 			},
 			want: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			localMockRepository, err := rules.NewLocalRepository()
-			if err != nil {
-				t.Fatal(err)
-			}
+			mockRepository := rules.NewRepository()
+			rule, _ := rules.Parse(tt.expression)
+			mockRepository.SetRules([]rules.Rule{rule})
 
 			s := service{
-				rulesRepository:  localMockRepository,
+				rulesRepository: mockRepository,
 			}
-			s.rulesRepository.SetCachedRules(tt.rules)
+
 			if got := s.evaluateRules(tt.args); got != tt.want {
 				t.Errorf("evaluateRules() = %v, want %v", got, tt.want)
 			}
--- rules/rules.go
+++ rules/rules.go
@@ -1,36 +1,88 @@
 package rules
 
 import (
+	"bufio"
+	"errors"
+	"os"
 	"regexp"
 
 	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
 )
 
 var (
-	reRuleSplitter = regexp.MustCompile(`(.+?)\s\"?(.+?)\"?\s\"(.+)\"`)
-
-	// DIRTY HACK: copied from filter/service.go (so we can verify rules are properly formatted while loading them); keep these regexes in sync!
-	filterEntryRegex = regexp.MustCompile(`(\w+?) (\S+?) (.+)`)
+	reRuleSplitter = regexp.MustCompile(`(.+?)\s\"?(.+?)\"?\s\"(\w+?) (\S+?) (.+)\"`)
 )
 
 // Repository defines the interface for the rules repository
 type Repository interface {
 	// FetchRules fetches the list of rules from a file or remote location
-	FetchRules(location string, l log.Logger) ([]Rule, error)
-
-	// RefreshRules refreshes the in-memory cached rules
-	RefreshRules(location string, l log.Logger) error
-
-	// SetCachedRules([]Rule)
-	SetCachedRules(rules []Rule)
-
-	// Rules returns rules from cache
+	FetchRules(location string, l log.Logger)
 	Rules() []Rule
+	SetRules(rules []Rule)
+}
+
+type repository struct {
+	rules []Rule
+}
+
+// NewLocalRepository returns a newly initialized rules repository
+func NewRepository() Repository {
+	return &repository{}
+}
+
+func (r *repository) Rules() []Rule {
+	return r.rules
+}
+
+func (r *repository) SetRules(rules []Rule) {
+	r.rules = rules
+}
+
+// FetchRules parses a local killfile to get all rules
+func (r *repository) FetchRules(location string, l log.Logger) {
+	file, err := os.Open(location)
+	if err != nil {
+		level.Error(l).Log("err", err)
+		return
+	}
+	defer file.Close()
+
+	var rules []Rule
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		rule, err := Parse(line)
+		if err != nil {
+			level.Error(l).Log("err", err, "expression", line)
+			continue
+		}
+
+		rules = append(rules, rule)
+	}
+	r.rules = rules
 }
 
 // Rule contains a killfile rule. There's no official standard so we implement these rules https://newsboat.org/releases/2.15/docs/newsboat.html#_killfiles
 type Rule struct {
-	Command          string
-	URL              string
-	FilterExpression string
+	Command   string
+	URL       string
+	Attribute string
+	Operator  string
+	Match     string
+}
+
+func Parse(line string) (Rule, error) {
+	matches := reRuleSplitter.FindStringSubmatch(line)
+	if len(matches) == 6 {
+		return Rule{
+			Command:   matches[1],
+			URL:       matches[2],
+			Attribute: matches[3],
+			Operator:  matches[4],
+			Match:     matches[5],
+		}, nil
+	} else {
+		return Rule{}, errors.New("invalid filter expression")
+	}
 }
